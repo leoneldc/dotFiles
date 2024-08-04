f11fdbd5a9830facfe4ab685904076803b22e82c
Leonel Dominguez <60021809+leoneldc@users.noreply.github.com>
2024-08-04 15:06:38 -0600

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,11 @@
+sudo apt update
+sudo apt upgrade
+clear
+
+sudo apt install bspwm sxhkd sddm feh kitty -y
+clear
+
+cp -r ./config/bspwm ~/.config/
+cp -r ./config/sxhkd ~/.config/
+cp -r ./config/kitty ~/.config/ 
+
